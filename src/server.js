24dcd8de2e716b50764df8f8622f652d59de5d5b
weggeeee <94621041+weggeeee@users.noreply.github.com>
2022-10-09 22:25:23 -0400

    Assignment 2
    initial website - 2 cookies (unique ID and time since last visit)

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 // use the express library
 const express = require('express');
 
+// use cookie parser
+const cookieParser = require('cookie-parser');
+
 // create a new server application
 const app = express();
+
+app.use(express.static('public'));
+app.use(cookieParser());
+app.set('view engine', 'ejs');
+
 
 // Define the port we will listen on
 // (it will attempt to read an environment global
@@ -10,11 +18,32 @@ const app = express();
 // world wide web).
 const port = process.env.PORT || 3000;
 
+// create vars
+let nextVisitorId = 1;
 
 // The main page of our website
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  let visitorId = req.cookies.visitorId ? nextVisitorId : ++nextVisitorId;
+  
+  res.cookie('visitorId', visitorId);
+  res.cookie('visited', dateNow = Date.now().toString());
+  
+  if (req.cookies.visited) {
+    req.cookies.visited = Math.floor((dateNow - req.cookies.visited ) / 1000)
+  } else {
+    req.cookies.visited = null;
+  }
+  
+  console.log(req.cookies.visited);
+  
+  res.render('welcome', {
+    name: req.query.name || "World",
+    date: new Date().toGMTString(),
+    visitorId: visitorId,
+    visitTime: req.cookies.visited, 
+  });
 });
+
 
 // Start listening for network connections
 app.listen(port);
